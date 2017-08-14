5230ef3af3f0d8bbf9bba97737fa7854d8ec1227
Shen <polossk@126.com>
2017-08-14 18:19:24 +0800

    regroup zoj

--- ZOJ/30 - 39/zoj3203.cpp
+++ ZOJ/30 - 39/zoj3203.cpp
@@ -18,6 +18,7 @@ inline int64  nextlld() { int64 d; scanf("%lld", &d); return d; }
 inline int64  next64d() { int64 d; scanf("%I64d",&d); return d; }
 
 const double eps = 1e-9;
+const double cef = (sqrt(5.0) - 1.0) * 0.5;
 double D, H, h;
 
 inline double calcf(double x)
@@ -25,32 +26,40 @@ inline double calcf(double x)
     return D - x + H - (H - h) * D / x;
 }
 
-inline bool test(double xl, double xr)
+inline bool test(double yl, double yr)
 {
-	// true : l = mid
-	// false: r = midmid
-	/**Specific Calculation**/
-	return calcf(xl) < calcf(xr);
+    // true : l = midl
+    // false: r = midr
+    /**Specific Calculation**/
+    return yl < yr;
 }
 
-double Tsearch_e(double l, double r)
+double Tsearch_s(double l, double r)
 {
     ///@return the x, not the f(x)
-    double midl = 0, midr = 0;
+    double midl = r - (r - l) * cef;
+    double midr = l + (r - l) * cef;
+    double ansl = calcf(midl), ansr = calcf(midr);
     while (r - l > eps)
     {
-        midl = (2 * l + r) / 3;
-        midr = (2 * r + l) / 3;
-        if (test(midl, midr)) l = midl;
-        else r = midr;
+        if (test(ansl, ansr))
+        {
+            l = midl; midl = midr; ansl = ansr;
+            midr = l + r - midl; ansr = calcf(midr);
+        }
+        else
+        {
+            r = midr; midr = midl; ansr = ansl;
+            midl = l + r - midr; ansl = calcf(midl);
+        }
     }
-    return midl;
+    return midr;
 }
 
 void solve()
 {
     H = nextDbf(); h = nextDbf(); D = nextDbf();
-    double x = Tsearch_e((H - h) * D / H, D);
+    double x = Tsearch_s((H - h) * D / H, D);
     printf("%.3lf\n", calcf(x));
 }
 
